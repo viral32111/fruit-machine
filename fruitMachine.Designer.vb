7771d75cb0ac951c28552ae102a39c405dd4231d
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-08-04 12:20:48 +0100

    Add original project from 2018

---
+++ fruitMachine.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fruitMachine
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(fruitMachine))
+		Me.btnSpin = New System.Windows.Forms.Button()
+		Me.lblBank = New System.Windows.Forms.Label()
+		Me.lblCredit = New System.Windows.Forms.Label()
+		Me.btnHoldOne = New System.Windows.Forms.Button()
+		Me.btnHoldTwo = New System.Windows.Forms.Button()
+		Me.btnHoldThree = New System.Windows.Forms.Button()
+		Me.btn10p = New System.Windows.Forms.Button()
+		Me.btn50p = New System.Windows.Forms.Button()
+		Me.btn1Pound = New System.Windows.Forms.Button()
+		Me.pnlTop = New System.Windows.Forms.Panel()
+		Me.btnClose = New System.Windows.Forms.Button()
+		Me.picThree = New System.Windows.Forms.PictureBox()
+		Me.picTwo = New System.Windows.Forms.PictureBox()
+		Me.picOne = New System.Windows.Forms.PictureBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.pnlTop.SuspendLayout()
+		CType(Me.picThree, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.picTwo, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.picOne, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'btnSpin
+		'
+		Me.btnSpin.BackColor = System.Drawing.Color.LightCyan
+		Me.btnSpin.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btnSpin.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnSpin.Location = New System.Drawing.Point(15, 328)
+		Me.btnSpin.Name = "btnSpin"
+		Me.btnSpin.Size = New System.Drawing.Size(298, 40)
+		Me.btnSpin.TabIndex = 3
+		Me.btnSpin.TabStop = False
+		Me.btnSpin.Text = "Spin!"
+		Me.btnSpin.UseVisualStyleBackColor = False
+		'
+		'lblBank
+		'
+		Me.lblBank.AutoSize = True
+		Me.lblBank.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblBank.Location = New System.Drawing.Point(25, 234)
+		Me.lblBank.Name = "lblBank"
+		Me.lblBank.Size = New System.Drawing.Size(85, 23)
+		Me.lblBank.TabIndex = 4
+		Me.lblBank.Text = "Bank: £10"
+		'
+		'lblCredit
+		'
+		Me.lblCredit.AutoSize = True
+		Me.lblCredit.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblCredit.Location = New System.Drawing.Point(25, 276)
+		Me.lblCredit.Name = "lblCredit"
+		Me.lblCredit.Size = New System.Drawing.Size(90, 23)
+		Me.lblCredit.TabIndex = 5
+		Me.lblCredit.Text = "Credit: £0"
+		'
+		'btnHoldOne
+		'
+		Me.btnHoldOne.BackColor = System.Drawing.Color.LightCyan
+		Me.btnHoldOne.Enabled = False
+		Me.btnHoldOne.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btnHoldOne.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnHoldOne.Location = New System.Drawing.Point(15, 180)
+		Me.btnHoldOne.Name = "btnHoldOne"
+		Me.btnHoldOne.Size = New System.Drawing.Size(87, 29)
+		Me.btnHoldOne.TabIndex = 6
+		Me.btnHoldOne.TabStop = False
+		Me.btnHoldOne.Text = "Hold"
+		Me.btnHoldOne.UseVisualStyleBackColor = False
+		'
+		'btnHoldTwo
+		'
+		Me.btnHoldTwo.BackColor = System.Drawing.Color.LightCyan
+		Me.btnHoldTwo.Enabled = False
+		Me.btnHoldTwo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btnHoldTwo.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnHoldTwo.Location = New System.Drawing.Point(121, 180)
+		Me.btnHoldTwo.Name = "btnHoldTwo"
+		Me.btnHoldTwo.Size = New System.Drawing.Size(87, 29)
+		Me.btnHoldTwo.TabIndex = 7
+		Me.btnHoldTwo.TabStop = False
+		Me.btnHoldTwo.Text = "Hold"
+		Me.btnHoldTwo.UseVisualStyleBackColor = False
+		'
+		'btnHoldThree
+		'
+		Me.btnHoldThree.BackColor = System.Drawing.Color.LightCyan
+		Me.btnHoldThree.Enabled = False
+		Me.btnHoldThree.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btnHoldThree.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnHoldThree.Location = New System.Drawing.Point(226, 180)
+		Me.btnHoldThree.Name = "btnHoldThree"
+		Me.btnHoldThree.Size = New System.Drawing.Size(87, 29)
+		Me.btnHoldThree.TabIndex = 8
+		Me.btnHoldThree.TabStop = False
+		Me.btnHoldThree.Text = "Hold"
+		Me.btnHoldThree.UseVisualStyleBackColor = False
+		'
+		'btn10p
+		'
+		Me.btn10p.BackColor = System.Drawing.Color.LightCyan
+		Me.btn10p.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btn10p.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btn10p.Location = New System.Drawing.Point(240, 218)
+		Me.btn10p.Name = "btn10p"
+		Me.btn10p.Size = New System.Drawing.Size(64, 29)
+		Me.btn10p.TabIndex = 9
+		Me.btn10p.TabStop = False
+		Me.btn10p.Text = "10p"
+		Me.btn10p.TextAlign = System.Drawing.ContentAlignment.TopCenter
+		Me.btn10p.UseVisualStyleBackColor = False
+		'
+		'btn50p
+		'
+		Me.btn50p.BackColor = System.Drawing.Color.LightCyan
+		Me.btn50p.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btn50p.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btn50p.Location = New System.Drawing.Point(240, 253)
+		Me.btn50p.Name = "btn50p"
+		Me.btn50p.Size = New System.Drawing.Size(64, 29)
+		Me.btn50p.TabIndex = 12
+		Me.btn50p.TabStop = False
+		Me.btn50p.Text = "50p"
+		Me.btn50p.UseVisualStyleBackColor = False
+		'
+		'btn1Pound
+		'
+		Me.btn1Pound.BackColor = System.Drawing.Color.LightCyan
+		Me.btn1Pound.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btn1Pound.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btn1Pound.Location = New System.Drawing.Point(240, 287)
+		Me.btn1Pound.Name = "btn1Pound"
+		Me.btn1Pound.Size = New System.Drawing.Size(64, 29)
+		Me.btn1Pound.TabIndex = 13
+		Me.btn1Pound.TabStop = False
+		Me.btn1Pound.Text = "£1"
+		Me.btn1Pound.UseVisualStyleBackColor = False
+		'
+		'pnlTop
+		'
+		Me.pnlTop.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+		Me.pnlTop.Controls.Add(Me.btnClose)
+		Me.pnlTop.Location = New System.Drawing.Point(0, 0)
+		Me.pnlTop.Name = "pnlTop"
+		Me.pnlTop.Size = New System.Drawing.Size(324, 24)
+		Me.pnlTop.TabIndex = 14
+		'
+		'btnClose
+		'
+		Me.btnClose.BackColor = System.Drawing.Color.Red
+		Me.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.btnClose.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnClose.ForeColor = System.Drawing.Color.White
+		Me.btnClose.Location = New System.Drawing.Point(288, -1)
+		Me.btnClose.Name = "btnClose"
+		Me.btnClose.Size = New System.Drawing.Size(30, 27)
+		Me.btnClose.TabIndex = 15
+		Me.btnClose.TabStop = False
+		Me.btnClose.Text = "X"
+		Me.btnClose.TextAlign = System.Drawing.ContentAlignment.TopCenter
+		Me.btnClose.UseVisualStyleBackColor = False
+		'
+		'picThree
+		'
+		Me.picThree.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.picThree.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.picThree.Location = New System.Drawing.Point(226, 83)
+		Me.picThree.Name = "picThree"
+		Me.picThree.Size = New System.Drawing.Size(86, 86)
+		Me.picThree.TabIndex = 17
+		Me.picThree.TabStop = False
+		'
+		'picTwo
+		'
+		Me.picTwo.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.picTwo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.picTwo.Location = New System.Drawing.Point(122, 83)
+		Me.picTwo.Name = "picTwo"
+		Me.picTwo.Size = New System.Drawing.Size(86, 86)
+		Me.picTwo.TabIndex = 16
+		Me.picTwo.TabStop = False
+		'
+		'picOne
+		'
+		Me.picOne.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.picOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.picOne.Location = New System.Drawing.Point(16, 83)
+		Me.picOne.Name = "picOne"
+		Me.picOne.Size = New System.Drawing.Size(86, 86)
+		Me.picOne.TabIndex = 15
+		Me.picOne.TabStop = False
+		'
+		'Label1
+		'
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(16, 31)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(296, 49)
+		Me.Label1.TabIndex = 18
+		Me.Label1.Text = "FRUIT MACHINE"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'fruitMachine
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+		Me.ClientSize = New System.Drawing.Size(324, 380)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.picThree)
+		Me.Controls.Add(Me.picTwo)
+		Me.Controls.Add(Me.picOne)
+		Me.Controls.Add(Me.pnlTop)
+		Me.Controls.Add(Me.btn1Pound)
+		Me.Controls.Add(Me.btn50p)
+		Me.Controls.Add(Me.btn10p)
+		Me.Controls.Add(Me.btnHoldThree)
+		Me.Controls.Add(Me.btnHoldTwo)
+		Me.Controls.Add(Me.btnHoldOne)
+		Me.Controls.Add(Me.lblCredit)
+		Me.Controls.Add(Me.lblBank)
+		Me.Controls.Add(Me.btnSpin)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.MaximizeBox = False
+		Me.Name = "fruitMachine"
+		Me.ShowIcon = False
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Fruit Machine"
+		Me.pnlTop.ResumeLayout(False)
+		CType(Me.picThree, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.picTwo, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.picOne, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents btnSpin As Button
+	Friend WithEvents lblBank As Label
+	Friend WithEvents lblCredit As Label
+	Friend WithEvents btnHoldOne As Button
+	Friend WithEvents btnHoldTwo As Button
+	Friend WithEvents btnHoldThree As Button
+	Friend WithEvents btn10p As Button
+	Friend WithEvents btn50p As Button
+	Friend WithEvents btn1Pound As Button
+	Friend WithEvents pnlTop As Panel
+	Friend WithEvents btnClose As Button
+	Friend WithEvents picOne As PictureBox
+	Friend WithEvents picTwo As PictureBox
+	Friend WithEvents picThree As PictureBox
+	Friend WithEvents Label1 As Label
+End Class
---
+++ fruitMachine.vb
@@ -0,0 +1,195 @@
+﻿Public Class fruitMachine
+	' The three boolean variables for each hold button indicating their state.
+	' This also meets a design requirement saying I need 3 reels and 3 hold buttons.
+	Dim holdOne As Boolean = False
+	Dim holdTwo As Boolean = False
+	Dim holdThree As Boolean = False
+
+	' Credit and bank balance integer variables
+	Dim credit As Integer = 0
+	Dim bank As Integer = 10
+
+	' Bitmap array to hold all 8 fruit images.
+	Dim fruit = New Bitmap() {
+		My.Resources.Apple,
+		My.Resources.Orange,
+		My.Resources.Watermelon,
+		My.Resources.Banana,
+		My.Resources.Carrot,
+		My.Resources.Strawberry,
+		My.Resources.Grapes,
+		My.Resources.Pear
+	}
+
+	' Wait function that takes an integer value as a parameter.
+	'This Function just() creates a timer In the background that does Nothing until the timespan is met.
+	'(It just delays code execution when called)
+	Private Sub wait(ByVal interval As Integer)
+		Dim stopwatch As New Stopwatch ' Create a new variable with the stopwatch class.
+
+		stopwatch.Start() ' Start the stopwatch.
+
+		Do While stopwatch.ElapsedMilliseconds < interval ' Keep going until elapsed time is greater than the time we set.
+			Application.DoEvents() ' Keeps calling events so the program doesnt go into a "Not responding" state.
+		Loop
+
+		stopwatch.Stop() ' Stop that stopwatch/
+	End Sub
+
+	' Event handler for when the spin button is pressed. This is the spin code.
+	Private Sub btnSpin_Click(sender As Object, e As EventArgs) Handles btnSpin.Click
+		If (holdOne And holdTwo And holdThree) Then ' Check if all the reel buttons are held down.
+			MsgBox("Cannot spin while all reels are held.") ' If so, display a messagebox.
+			Return ' Stop running any code after this.
+		End If
+
+		' Split credit label string and substring it then select the second value in the array (Indexing starts at 0) then convert to datatype double.
+		Dim credit As Double = lblCredit.Text.Split("£")(1)
+		If (credit >= 0.2) Then ' Is the credit greater than 0.20?
+			Dim split As String() = lblCredit.Text.Split("£") ' Split the entire stirng this time into a string array.
+			Dim newCredit = split(1) - 0.2 ' Subtract the game fee from their current credit balance.
+			lblCredit.Text = "Credit: £" + newCredit.ToString() ' Update the label with their new credit balance.
+		Else
+			MsgBox("You don't have enough credit! Each spin is £0.20.") ' Tell them if they don't have enough to play.
+			Return ' Stop running any code after this.
+		End If
+
+		' Disable all the buttons so they don't touch anything while the reels spin.
+		btnSpin.Enabled = False
+		btnHoldOne.Enabled = False
+		btnHoldTwo.Enabled = False
+		btnHoldThree.Enabled = False
+		' These 3 buttons below meet the design requirement saying I need a way to enter different coins.
+		btn10p.Enabled = False
+		btn50p.Enabled = False
+		btn1Pound.Enabled = False
+
+		' Create a new random class variable and 3 integer variables that are empty.
+		Dim rand As Random = New Random()
+		Dim randOne As Integer
+		Dim randTwo As Integer
+		Dim randThree As Integer
+
+		' Iterate 0 to 100. This will be for animations on the reels
+		For num As Integer = 0 To 100
+			If (holdOne = False) Then ' Is the first reel not being held?
+				randOne = rand.Next(0, 7) ' New number between 0 and 7 (Its actually 1 and 8 but .Next starts at 0)
+				picOne.BackgroundImage = fruit(randOne) ' Get the image that corrisponds to the random integer index in the array.
+			End If
+
+			If (holdTwo = False) Then ' Is the second reel not being held?
+				randTwo = rand.Next(0, 7) ' New number between 0 and 7 (Its actually 1 and 8 but .Next starts at 0)
+				picTwo.BackgroundImage = fruit(randTwo) ' Get the image that corrisponds to the random integer index in the array.
+			End If
+
+			If (holdThree = False) Then ' Is the third reel not being held?
+				randThree = rand.Next(0, 7) ' New number between 0 and 7 (Its actually 1 and 8 but .Next starts at 0)
+				picThree.BackgroundImage = fruit(randThree) ' Get the image that corrisponds to the random integer index in the array.
+			End If
+
+			wait(num / 1.1) ' Delay the for loop, this adds the animation and makes it look like it slows down towards the end since it divides the iterating number by 1.1
+		Next
+
+		' Do all numbers match?
+		If (randOne = randTwo And randThree = randTwo) Then
+			MsgBox("You've won £1") ' Tell them they have won. This also meets the requirement saying they need to be shown they won.
+
+			' Add money to their bank balance.
+			Dim bank As Double = lblBank.Text.Split("£")(1)
+			bank += 1
+			lblBank.Text = "Bank: £" + bank.ToString()
+		End If
+
+		' Renable all the buttons so they can play again.
+		btnSpin.Enabled = True
+		btnHoldOne.Enabled = True
+		btnHoldTwo.Enabled = True
+		btnHoldThree.Enabled = True
+		btn10p.Enabled = True
+		btn50p.Enabled = True
+		btn1Pound.Enabled = True
+	End Sub
+
+	' Event handler for when the third hold button is pressed. This holds the third reel.
+	Private Sub btnHoldThree_Click(sender As Object, e As EventArgs) Handles btnHoldThree.Click
+		If (holdThree) Then ' Is it pressed?
+			btnHoldThree.BackColor = Color.LightCyan ' Update back color so it is no longer visually pressed.
+			holdThree = False ' Set it being pressed to no.
+		Else
+			btnHoldThree.BackColor = Color.LightCoral ' Update back color so it looks like its pressed.
+			holdThree = True ' Set it being pressed to yes.
+		End If
+	End Sub
+
+	' Event handler for when the second hold button is pressed. This holds the second reel.
+	Private Sub btnHoldTwo_Click(sender As Object, e As EventArgs) Handles btnHoldTwo.Click
+		If (holdTwo) Then ' Is it pressed?
+			btnHoldTwo.BackColor = Color.LightCyan ' Update back color so it is no longer visually pressed.
+			holdTwo = False ' Set it being pressed to no.
+		Else
+			btnHoldTwo.BackColor = Color.LightCoral ' Update back color so it looks like its pressed.
+			holdTwo = True ' Set it being pressed to yes.
+		End If
+	End Sub
+
+	' Event handler for when the first hold button is pressed. This holds the first reel.
+	Private Sub btnHoldOne_Click(sender As Object, e As EventArgs) Handles btnHoldOne.Click
+		If (holdOne) Then ' Is it pressed?
+			btnHoldOne.BackColor = Color.LightCyan ' Update back color so it is no longer visually pressed.
+			holdOne = False ' Set it being pressed to no.
+		Else
+			btnHoldOne.BackColor = Color.LightCoral ' Update back color so it looks like its pressed.
+			holdOne = True ' Set it being pressed to yes.
+		End If
+	End Sub
+
+	' Event handler for when the close button is pressed. This closes the application.
+	Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+		Close() ' Close the entire application safley.
+	End Sub
+
+	' Event hander for when the add £1 button is pressed. This takes money from the bank and puts it in credit.
+	Private Sub btn1Pound_Click(sender As Object, e As EventArgs) Handles btn1Pound.Click
+		Dim bank As Double = lblBank.Text.Split("£")(1) ' Split string and select second value
+		If (bank >= 1) Then ' Is there balance greater than £1?
+			' Update their credit with £1 added and subtracted from bank
+			Dim split As String() = lblCredit.Text.Split("£")
+			Dim credit = split(1) + 1
+			lblCredit.Text = "Credit: £" + credit.ToString() ' This meets the requirement saying I need a credit field.
+			bank -= 1
+			lblBank.Text = "Bank: £" + bank.ToString() ' This meets the requirement saying I need a bank field
+		Else
+			MsgBox("You don't have enough funds!") ' Tell them they don't have enough funds in the bank
+		End If
+	End Sub
+
+	' Event hander for when the add 10p button is pressed. This takes money from the bank and puts it in credit.
+	Private Sub btn10p_Click(sender As Object, e As EventArgs) Handles btn10p.Click
+		Dim bank As Double = lblBank.Text.Split("£")(1) ' Split string and select second value
+		If (bank >= 0.1) Then ' Is there balance greater than £0.10?
+			' Update their credit with £0.10 added and subtracted from bank
+			Dim split As String() = lblCredit.Text.Split("£")
+			Dim credit = split(1) + 0.1
+			lblCredit.Text = "Credit: £" + credit.ToString()
+			bank -= 0.1
+			lblBank.Text = "Bank: £" + bank.ToString()
+		Else
+			MsgBox("You don't have enough funds!") ' Tell them they don't have enough funds in the bank
+		End If
+	End Sub
+
+	' Event hander for when the add 50p button is pressed. This takes money from the bank and puts it in credit.
+	Private Sub btn50p_Click(sender As Object, e As EventArgs) Handles btn50p.Click
+		Dim bank As Double = lblBank.Text.Split("£")(1) ' Split string and select second value
+		If (bank >= 0.5) Then ' Is there balance greater than £0.50?
+			' Update their credit with £0.50 added and subtracted from bank
+			Dim split As String() = lblCredit.Text.Split("£")
+			Dim credit = split(1) + 0.5
+			lblCredit.Text = "Credit: £" + credit.ToString()
+			bank -= 0.5
+			lblBank.Text = "Bank: £" + bank.ToString()
+		Else
+			MsgBox("You don't have enough funds!") ' Tell them they don't have enough funds in the bank
+		End If
+	End Sub
+End Class
